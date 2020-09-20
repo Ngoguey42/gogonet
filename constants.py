963241fb5b461a5f82fd3124b4aecb35332e85ac
Ngoguey42 <ngoguey@student.42.fr>
2020-09-20 15:42:33 +0200

    Update tools to support breaks, and constants for the 6 games

--- constants.py
+++ constants.py
@@ -56,11 +56,30 @@ GAMES=(
         idx_in_encounter=0,
         minimap_count=2,
         round_count=34,
-        vod_anchors={
-            # ("round_freeze_end", 1): _t("00:03:07.281"),
-            # ("round_freeze_end", ): _t(""),
-            # ("round_freeze_end", ): _t(""),
-        },
+        vod_anchors=(
+
+            # ("clap", "round_freeze_end", 0 + 0, occluded
+            ("clap", "round_first_displacement", 0 + 1, _t("00:03:07.247")),
+            ("clap", "round_first_displacement", 4 + 3, _t("00:14:48.865")),
+            ("clap", "round_first_displacement", 8 + 4, _t("00:22:34.230")),
+            # ("clap", "round_freeze_end", 9 + 4, occluded
+            # ("clap", "round_freeze_end", 10 + 4, occluded
+
+            ("break", "round_start", 11 + 4),
+            ("clap", "round_first_displacement", 11 + 4, _t("00:31:52.254")), # 15
+            # ("clap", "round_first_displacement", 12 + 7, _t("00:39:59.874")), # 19
+            # ("clap", "round_first_displacement", 13 + 7, _t("00:42:14.325")), # 20
+            ("clap", "round_first_displacement", 14 + 7, _t("00:44:30.311")), # 21
+            # ("clap", "round_first_displacement", 22 occluded
+            # ("clap", "round_first_displacement", 23 occluded
+            ("clap", "round_first_displacement", 15 + 14, _t("00:59:41.221")), # 29
+
+            ("break", "round_start", 15 + 15),
+
+            ("clap", "round_first_displacement", 15 + 15, _t("01:06:31.281")),
+            ("clap", "round_first_displacement", 15 + 16, _t("01:08:22.359")),
+            ("clap", "round_first_displacement", 15 + 18, _t("01:12:36.045")),
+        ),
     ),
     dict(
         **ENCOUNTERS["2343670_big-vs-godsent-esl-pro-league-season-12-europe"],
@@ -68,11 +87,14 @@ GAMES=(
         idx_in_encounter=1,
         minimap_count=1,
         round_count=41,
-        vod_anchors={
-            ("round_freeze_end", 1): _t("01:32:35.277"),
-            ("round_freeze_end", 20): _t("02:16:37.233"),
-            ("round_freeze_end", 40): _t("02:58:59.623"),
-        },
+        vod_anchors=(
+            # ("clap", "round_freeze_end", 1, _t("01:32:35.277")), # on overlay
+            # ("clap", "round_freeze_end", 20, _t("02:16:37.233")), # on overlay
+            # ("clap", "round_freeze_end", 40, _t("02:58:59.623")), # on overlay
+            ("clap", "round_first_displacement", 1, _t("01:32:35.210")),
+            ("clap", "round_first_displacement", 20, _t("02:16:37.166")),
+            ("clap", "round_first_displacement", 40, _t("02:58:59.556")),
+        ),
     ),
     dict(
         **ENCOUNTERS["2343670_big-vs-godsent-esl-pro-league-season-12-europe"],
@@ -80,6 +102,11 @@ GAMES=(
         idx_in_encounter=2,
         minimap_count=2,
         round_count=23,
+        vod_anchors=(
+            ("clap", "round_first_displacement", 1 + 0, _t("03:20:13.896")),
+            ("clap", "round_first_displacement", 11 + 2, _t("03:43:58.936")),
+            ("clap", "round_first_displacement", 15 + 7, _t("04:01:39.178")),
+        ),
     ),
 
     dict(
@@ -88,11 +115,14 @@ GAMES=(
         idx_in_encounter=0,
         minimap_count=1,
         round_count=42,
-        vod_anchors={
-            ("round_freeze_end", 1): _t("00:06:49.088"),
-            ("round_freeze_end", 21): _t("00:46:08.548"),
-            ("round_freeze_end", 41): _t("01:26:41.548"),
-        },
+        vod_anchors=(
+            # ("clap", "round_freeze_end", 1, _t("00:06:49.088")), # on overlay
+            # ("clap", "round_freeze_end", 21, _t("00:46:08.548")), # on overlay
+            # ("clap", "round_freeze_end", 41, _t("01:26:41.548")), # on overlay
+            ("clap", "round_first_displacement", 1, _t("00:06:49.128")),
+            ("clap", "round_first_displacement", 21, _t("00:46:08.608")),
+            ("clap", "round_first_displacement", 41, _t("01:26:41.588")),
+        ),
     ),
     dict(
         **ENCOUNTERS["2343922_gambit-youngsters-vs-sprout-nine-to-five-4"],
@@ -100,6 +130,17 @@ GAMES=(
         idx_in_encounter=1,
         minimap_count=1,
         round_count=30,
+        vod_anchors=(
+            # ("clap", "round_first_displacement", 0 + 0, hard to say
+            # ("clap", "round_first_displacement", 0 + 1, occluded
+            ("clap", "round_first_displacement", 0 + 2, _t("01:49:44.768")),
+            ("clap", "round_first_displacement", 2 + 9, _t("02:08:44.268")),
+            # 5+10 is visible, but the 3 following are not
+            ("clap", "round_first_displacement", 8 + 11, _t("02:26:12.508")),
+            ("clap", "round_first_displacement", 13 + 12, _t("02:37:54.488")),
+            ("clap", "round_first_displacement", 15 + 13, _t("02:44:57.768")),
+            # ("clap", "round_first_displacement", 15 + 14, occluded
+        ),
     ),
     dict(
         **ENCOUNTERS["2343922_gambit-youngsters-vs-sprout-nine-to-five-4"],
@@ -107,12 +148,45 @@ GAMES=(
         idx_in_encounter=2,
         minimap_count=1,
         round_count=24,
+        vod_anchors=(
+            # ("clap", "round_first_displacement", 0 + 0, _t("03:00:15.028")), # not in dem
+            # ("clap", "round_first_displacement", 1 + 0, occluded
+
+            ("clap", "round_first_displacement", 2 + 0, _t("03:02:01.228")),
+            ("clap", "round_first_displacement", 4 + 7, _t("03:18:24.428")),
+            ("clap", "round_first_displacement", 6 + 11, _t("03:31:17.688")),
+            ("clap", "round_first_displacement", 8 + 15, _t("03:42:59.948")),
+        ),
     ),
 )
 GAMES = _toser({
     (d["ename"], d["idx_in_encounter"]): d
     for d in GAMES
 })
+
+def _test():
+    for g in GAMES:
+        if "vod_anchors" not in g.index:
+            continue
+        clap_times = [
+            a[3]
+            for a in g.vod_anchors
+            if a[0] == "clap"
+        ]
+        for t, u in zip(clap_times, clap_times[1:]):
+            assert t + 1 < u, (g.mname, g.ename, clap_times)
+        round_idxs = [
+            a[2]
+            for a in g.vod_anchors
+        ]
+        for t, u in zip(round_idxs, round_idxs[1:]):
+            assert t <= u, (g.mname, g.ename, round_idxs)
+        events = [
+            (a[1], a[2])
+            for a in g.vod_anchors
+        ]
+        assert len(set(events)) == len(events), (g.mname, g.ename, events)
+_test()
 
 DB_PREFIX = {
     "/": "/mnt/y/d/csgo",
--- tools.py
+++ tools.py
@@ -64,15 +64,15 @@ def path_of_vod(ename, egidx):
 
 def load_json(ename, egidx):
     path = path_of_json(ename, egidx)
-    print("reading", path)
+    # print("reading", path)
     j = open(path).read()
 
-    print("parsing json")
+    # print("parsing json")
     j = json.loads(j)
-    print("slicing json")
+    # print("slicing json")
     l = [ev for ev in j if ev["ev"] == "tickend"]
 
-    print("making df")
+    # print("making df")
     rows = []
     for ev in l:
         for pid, (x, y, z, alive) in ev["pinfo"].items():
@@ -82,7 +82,7 @@ def load_json(ename, egidx):
                 x=x, y=y, z=z, t=ev["t"], pid=pid,
             ))
     dfticks = pd.DataFrame(rows).set_index(["t", "pid"], verify_integrity=True)
-    print(dfticks)
+    # print(dfticks)
 
 
     rows = []
@@ -93,56 +93,147 @@ def load_json(ename, egidx):
             for k, v in r.items()
         ])
     dfevs = pd.DataFrame(rows).set_index("t", verify_integrity=True)
-    print(dfevs)
+    # print(dfevs)
 
     return dfticks, dfevs
 
 def create_timestamp_conversions(dfevs, anchors):
     df = dfevs.reset_index().set_index(["ev", "round_idx"])
-    anchors = [
-        dict(tvod=t, tdem=df.loc[k, "t"])
-        for k, t in anchors.items()
-    ]
-    def to_dem(tvod):
-        if tvod < anchors[0]["tvod"]:
-            left_anchor, right_anchor = anchors[:2]
-        elif tvod > anchors[-1]["tvod"]:
-            left_anchor, right_anchor = anchors[-2:]
-        else:
-            for left_anchor, right_anchor in zip(anchors, anchors[1:]):
-                if left_anchor["tvod"] <= tvod <= right_anchor["tvod"]:
-                    break
-            else:
-                assert False, "unreachable"
-        root = left_anchor["tvod"]
-        span = right_anchor["tvod"] - root
-        assert span > 0
-        fraction = (tvod - root) / span
 
-        root = left_anchor["tdem"]
-        span = right_anchor["tdem"] - root
-        assert span > 0
-        return root + fraction * span
+    def get_tdem(ev, round_idx):
+        """Find the event in df"""
+        if ev == "round_first_displacement":
+            ev = "round_freeze_end" # TODO: Find the time displacement between those 2 events
+        return float(df.loc[(ev, round_idx), "t"])
+
+    def create_lerp_on_floats(xleft, xright, yleft, yright):
+        """Linear interpolation and extrapolation"""
+        rootx = xleft
+        spanx = xright - rootx
+        assert spanx > 0
+        rooty = yleft
+        spany = yright - rooty
+        assert spany > 0
+        def lerp(x):
+            fractionx = (x - rootx) / spanx
+            return rooty + fractionx * spany
+        return lerp
+
+    def yield_3_by_3(l):
+        for i in range(len(l) - 2):
+            yield l[i:i+3]
+
+    anchors = [["start"]] + list(anchors) + [["end"]]
+    tdem_intervals, tvod_intervals, to_dems, to_vods = [], [], [], []
+    for a0, a1, a2 in yield_3_by_3(anchors):
+        tag = (a0[0], a1[0], a2[0])
+        if tag == ("start", "clap", "clap"):
+            # ]-inf; a1], lerp on a1/a2
+            left, right = a1, a2
+            to_dem = create_lerp_on_floats(
+                left[3], right[3],
+                get_tdem(left[1], left[2]), get_tdem(right[1], right[2]),
+            )
+            to_vod = create_lerp_on_floats(
+                get_tdem(left[1], left[2]), get_tdem(right[1], right[2]),
+                left[3], right[3],
+            )
+            tdem_start = -np.inf
+            tdem_end = get_tdem(a1[1], a1[2])
+            tvod_start = -np.inf
+            tvod_end = a1[3]
+        elif tag == ("clap", "clap", "clap"):
+            # [a0; a1], lerp on a0/a1
+            left, right = a0, a1
+            to_dem = create_lerp_on_floats(
+                left[3], right[3],
+                get_tdem(left[1], left[2]), get_tdem(right[1], right[2]),
+            )
+            to_vod = create_lerp_on_floats(
+                get_tdem(left[1], left[2]), get_tdem(right[1], right[2]),
+                left[3], right[3],
+            )
+            tdem_start = get_tdem(a0[1], a0[2])
+            tdem_end = get_tdem(a1[1], a1[2])
+            tvod_start = a0[3]
+            tvod_end = a1[3]
+        elif tag == ("clap", "clap", "break"):
+            # [a0; break], lerp on a0/a1
+            left, right = a0, a1
+            to_dem = create_lerp_on_floats(
+                left[3], right[3],
+                get_tdem(left[1], left[2]), get_tdem(right[1], right[2]),
+            )
+            to_vod = create_lerp_on_floats(
+                get_tdem(left[1], left[2]), get_tdem(right[1], right[2]),
+                left[3], right[3],
+            )
+            tdem_start = get_tdem(a0[1], a0[2])
+            tdem_end = get_tdem(a2[1], a2[2])
+            tvod_start = a0[3]
+            tvod_end = to_vod(tdem_end) # extrapolated from the current lerp
+        elif tag == ("clap", "break", "clap"):
+            continue # covered by previous case
+        elif tag == ("break", "clap", "clap"):
+            # [break; a1], lerp on a1/a2
+            left, right = a1, a2
+            to_dem = create_lerp_on_floats(
+                left[3], right[3],
+                get_tdem(left[1], left[2]), get_tdem(right[1], right[2]),
+            )
+            to_vod = create_lerp_on_floats(
+                get_tdem(left[1], left[2]), get_tdem(right[1], right[2]),
+                left[3], right[3],
+            )
+            tdem_start = get_tdem(a0[1], a0[2])
+            tdem_end = get_tdem(a1[1], a1[2])
+            tvod_start = to_vods[-1](tdem_start) # extrapolated from the previous lerp
+            tvod_end = a1[3]
+        elif tag == ("clap", "clap", "end"):
+            # [a0; +inf[, lerp on a0/a1
+            left, right = a0, a1
+            to_dem = create_lerp_on_floats(
+                left[3], right[3],
+                get_tdem(left[1], left[2]), get_tdem(right[1], right[2]),
+            )
+            to_vod = create_lerp_on_floats(
+                get_tdem(left[1], left[2]), get_tdem(right[1], right[2]),
+                left[3], right[3],
+            )
+            tdem_start = get_tdem(a0[1], a0[2])
+            tdem_end = +np.inf
+            tvod_start = a0[3]
+            tvod_end = +np.inf
+        else:
+            assert False, tag
+        # print("> ", tag)
+        if tdem_start != -np.inf:
+            assert tdem_intervals[-1][1] == tdem_start, (tdem_intervals[-1][1], tdem_start)
+            assert tvod_intervals[-1][1] == tvod_start, (tvod_intervals[-1][1], tvod_start)
+        tdem_intervals.append((tdem_start, tdem_end))
+        tvod_intervals.append((tvod_start, tvod_end))
+        to_dems.append(to_dem)
+        to_vods.append(to_vod)
+
+    assert tdem_intervals[0][0] == -np.inf
+    assert tdem_intervals[-1][1] == np.inf
+    assert tvod_intervals[0][0] == -np.inf
+    assert tvod_intervals[-1][1] == np.inf
+
+    def to_dem(tvod):
+        """Not monotonic because of breaks, surjective
+        """
+        for i, (start, end) in enumerate(tvod_intervals):
+            if start <= tvod <= end:
+                return to_dems[i](tvod)
+        assert False, "unreachable"
 
     def to_vod(tdem):
-        if tdem < anchors[0]["tdem"]:
-            left_anchor, right_anchor = anchors[:2]
-        elif tdem > anchors[-1]["tdem"]:
-            left_anchor, right_anchor = anchors[-2:]
-        else:
-            for left_anchor, right_anchor in zip(anchors, anchors[1:]):
-                if left_anchor["tdem"] <= tdem <= right_anchor["tdem"]:
-                    break
-            else:
-                assert False, "unreachable"
-        root = left_anchor["tdem"]
-        span = right_anchor["tdem"] - root
-        assert span > 0
-        fraction = (tdem - root) / span
-
-        root = left_anchor["tvod"]
-        span = right_anchor["tvod"] - root
-        assert span > 0
-        return root + fraction * span
+        """Monotonic, injective
+        """
+        for i, (start, end) in enumerate(tdem_intervals):
+            if start <= tdem <= end:
+                return to_vods[i](tdem)
+        assert False, "unreachable"
 
     return to_vod, to_dem
